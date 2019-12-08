4e40fec213edc70101dc4e91c200deeda01d040b
Chris <cjmarsh725@gmail.com>
2019-12-07 18:37:39 -0800

    Implemented start script for watching src files

---
+++ src/game.js
@@ -0,0 +1,9 @@
+let app = new PIXI.Application({ 
+  width: 800,         // default: 800
+  height: 600,        // default: 600
+  antialias: true,    // default: false
+  transparent: false, // default: false
+  resolution: 1       // default: 1
+});
+
+document.body.appendChild(app.view);
